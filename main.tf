cc0dd4465d86d2d6133c81c54afdeba26ce091bb
daveysteve <stephen.davey2@coles.com.au>
2022-03-25 03:00:03 +0000

    modulee

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "myresourcegroup" {
-  name     = "rg_daveysteve"
+  name     = "${var.prefix}-workshop"
   location = var.location
 
   tags = {
--- network.tf
+++ network.tf
@@ -2,7 +2,7 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "rg_daveysteve" {
   name     = "my-resources"
   location = "West Europe"
 }
@@ -10,8 +10,8 @@ resource "azurerm_resource_group" "example" {
 module "network" {
   source  = "app.terraform.io/teraform-daveysteve/network/azurerm"
   version = "3.5.0"
-  resource_group_name = "rg_daveysteve"
-  #resource_group_name = azurerm_resource_group.example.name
+  #resource_group_name = "rg_daveysteve"
+  resource_group_name = azurerm_resource_group.rg_daveysteve.name
   address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
@@ -27,6 +27,6 @@ module "network" {
     costcenter  = "it"
   }
 
-  depends_on = [azurerm_resource_group.example]
+  depends_on = [azurerm_resource_group.rg_daveysteve]
 }
 
