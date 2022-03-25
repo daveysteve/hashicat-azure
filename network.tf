201d8a2362698848cc84ab7a9c6f191f36adb186
daveysteve <stephen.davey2@coles.com.au>
2022-03-25 01:53:47 +0000

    add network module

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 module "network" {
-#  source  = "app.terraform.io/teraform-daveysteve/network/azurerm"
+###  source  = "app.terraform.io/teraform-daveysteve/network/azurerm"
   source  = "Azure/network/azurerm"
   version = "3.5.0"
   resource_group_name = "rg_daveysteve"
